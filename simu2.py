1f9f9b95583e00f3d04c43b535a5af37b690fc0a
= <=>
2024-02-23 17:11:36 +0100

    simu2 pas raccord pour l'instant

--- simu2.py
+++ simu2.py
@@ -14,7 +14,7 @@ e_absorption, max_order = pra.inverse_sabine(rt60, room_dim)
 
 # Create the room
 room = pra.ShoeBox(
-    room_dim, fs=16000, materials=pra.Material(e_absorption), max_order=max_order
+    room_dim, fs=44100, materials=pra.Material(e_absorption), max_order=max_order
 )
 
 
@@ -23,11 +23,11 @@ room = pra.ShoeBox(
 _, audio = wavfile.read('test3_short.wav')
 
 # place the source in the room
-room.add_source([2.5, 3.73, 1.76], signal=audio, delay=1.3)
+room.add_source([2.5, 3.73, 1.76], signal=audio, delay=1.5)
 
 # define the locations of the microphones
 mic_locs = np.c_[
-    [6.3, 4.87, 1.2],  # mic 1
+    [3, 4, 2],  # mic 1
     [6.3, 4.93, 1.2],  # mic 2
 ]
 
@@ -36,9 +36,9 @@ room.add_microphone_array(mic_locs)
 
 room.compute_rir()
 
-# plot the RIR between mic 1 and source 0
-plt.plot(room.rir[1][0])
-plt.show()
+# # plot the RIR between mic 1 and source 0
+# plt.plot(room.rir[1][0])
+# plt.show()
 
 
 sf.write("test_result_simu2.wav", room.rir[1][0], 44100)
