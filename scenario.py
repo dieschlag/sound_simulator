351c704114b62046e034e0f8221f08d3c677836d
mateo.dischler <mateo.dischler@student-cs.fr>
2024-03-27 05:44:50 +0100

    added simu function

--- scenario.py
+++ scenario.py
@@ -25,6 +25,8 @@ vitesse_son = 343
 fs = 44100
 snr_db = 40
 
+simu(dimensions_piece, absorption)
+
 
 """
 -------------------------------------------------------------------------------------------
@@ -32,31 +34,31 @@ Ne pas modifier
 -------------------------------------------------------------------------------------------
 """
 
-chemin_du_dossier = input(
-"Entrez le chemin du dossier contenant les fichiers audio : ")
+# chemin_du_dossier = input(
+# "Entrez le chemin du dossier contenant les fichiers audio : ")
 
-fichiers_audio = lister_fichiers(chemin_du_dossier)
-signaux = [audio_to_signal(chemin_du_dossier, position_sources[i][1] + ".wav")
-           for i in range(len(position_sources))]
+# fichiers_audio = lister_fichiers(chemin_du_dossier)
+# signaux = [audio_to_signal(chemin_du_dossier, position_sources[i][1] + ".wav")
+#            for i in range(len(position_sources))]
 
-# Trouver la longueur maximale parmi tous les signaux
-longueur_max = int(np.min([len(signal) for signal in signaux]))
+# # Trouver la longueur maximale parmi tous les signaux
+# longueur_max = int(np.min([len(signal) for signal in signaux]))
 
-# Ajuster la longueur de tous les signaux à la longueur_max
-signaux_ajustes = ajuster_longueur_signaux(signaux, longueur_max)
+# # Ajuster la longueur de tous les signaux à la longueur_max
+# signaux_ajustes = ajuster_longueur_signaux(signaux, longueur_max)
 
-all_signal = signaux_ajustes
-sources= [item[0] for item in position_sources]
-print(all_signal)
-print(sources)
-print(absorption)
+# all_signal = signaux_ajustes
+# sources= [item[0] for item in position_sources]
+# print(all_signal)
+# print(sources)
+# print(absorption)
 
-ri_piece = ri(absorption, dimensions_piece, sources, positions_micros)
+# ri_piece = ri(absorption, dimensions_piece, sources, positions_micros)
 
-simulateur = Simulateur(positions_micros, position_sources, all_signal, ri_piece)
-signaux_micros = simulateur.simuler_microphones(position_sources)
-liste_des_signaux = signaux_micros
-taux_echantillonnage = 44100
-creer_dossier_et_enregistrer_signaux(
-    "Signaux_micros", liste_des_signaux, taux_echantillonnage)
+# simulateur = Simulateur(positions_micros, position_sources, all_signal, ri_piece)
+# signaux_micros = simulateur.simuler_microphones(position_sources)
+# liste_des_signaux = signaux_micros
+# taux_echantillonnage = 44100
+# creer_dossier_et_enregistrer_signaux(
+#     "Signaux_micros", liste_des_signaux, taux_echantillonnage)
 
--- simu1.py
+++ simu1.py
@@ -8,6 +8,7 @@ import sys
 import os
 import wave
 import glob
+from reverb import *
 
 positions_micros = []
 position_sources = []
@@ -161,13 +162,37 @@ def creer_dossier_et_enregistrer_signaux(nom_dossier, signaux, taux_echantillonn
         sf.write(nom_fichier, signal, taux_echantillonnage)
         print(f"Signal {i+1} enregistré sous : {nom_fichier}")
 
-# Exemple d'utilisation
-# `taux_echantillonnage` doit correspondre au taux d'échantillonnage des signaux contenus dans `liste_des_signaux`
+def simu (dimensions_piece, absorption):
+
+    dimensions_piece = piece()
+    absorption = absorption_coeff()
+
+    chemin_du_dossier = input(
+    "Entrez le chemin du dossier contenant les fichiers audio : ")
+
+    fichiers_audio = lister_fichiers(chemin_du_dossier)
+    signaux = [audio_to_signal(chemin_du_dossier, position_sources[i][1] + ".wav")
+            for i in range(len(position_sources))]
+
+    # Trouver la longueur maximale parmi tous les signaux
+    longueur_max = int(np.min([len(signal) for signal in signaux]))
+
+    # Ajuster la longueur de tous les signaux à la longueur_max
+    signaux_ajustes = ajuster_longueur_signaux(signaux, longueur_max)
+
+    all_signal = signaux_ajustes
+    sources= [item[0] for item in position_sources]
+    print(all_signal)
+    print(sources)
+    print(absorption)
+
+    ri_piece = ri(absorption, dimensions_piece, sources, positions_micros)
+
+    simulateur = Simulateur(positions_micros, position_sources, all_signal, ri_piece)
+    signaux_micros = simulateur.simuler_microphones(position_sources)
+    liste_des_signaux = signaux_micros
+    taux_echantillonnage = 44100
+    creer_dossier_et_enregistrer_signaux(
+        "Signaux_micros", liste_des_signaux, taux_echantillonnage)
 
 
-# liste_des_signaux = signaux_micros  # Votre liste de signaux ici
-# # Taux d'échantillonnage des signaux (à ajuster selon vos données)
-# taux_echantillonnage = 44100
-
-# creer_dossier_et_enregistrer_signaux(
-#     "Signaux_micros", liste_des_signaux, taux_echantillonnage)
