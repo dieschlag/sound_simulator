d44cb36a0fa3fe43da1ed07020b18262239c9af3
mateo.dischler <mateo.dischler@student-cs.fr>
2024-03-27 10:04:42 +0100

    added two scenarios to test the programme

---
+++ scenario1.py
@@ -0,0 +1,15 @@
+from simulateur import *
+
+source(2.5, 5, 1, "sources/Audio.wav")
+
+micro(2, 1, 1.5, 40)
+micro(2.25, 1, 1.5, 40)
+micro(2.5, 1, 1.5, 40)
+micro(2.75, 1, 1.5, 40)
+micro(3, 1, 1.5, 40)
+
+piece(5, 10, 2)
+absorption_coeff(east=0.2, west=0.2, north=0.2, south=0.2, ceiling=0.6, floor=0.3)
+vitesse_son(343)
+
+simuler()
---
+++ scenario2.py
@@ -0,0 +1,17 @@
+
+from simulateur import *
+
+source(2.5, 4.5, 1, "sources/Audio1.wav")
+source(2.5, 5.5, 1, "sources/Audio2.wav")
+
+micro(2, 1, 1.5, 40)
+micro(2.25, 1, 1.5, 40)
+micro(2.5, 1, 1.5, 40)
+micro(2.75, 1, 1.5, 40)
+micro(3, 1, 1.5, 40)
+
+piece(5, 10, 2)
+absorption_coeff(east=0.2, west=0.2, north=0.2, south=0.2, ceiling=0.6, floor=0.3)
+vitesse_son(343)
+
+simuler()
